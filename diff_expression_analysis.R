ccd793d6c7db827e70797728a0568c1de8cd2160
stephaniesaab <stephaniesaab5@gmail.com>
2026-02-22 14:13:50 -0500

    Adding DE Rscript

---
+++ diff_expression_analysis.R
@@ -0,0 +1,13 @@
+#Assignment 2: Transcriptomics:
+library("airway")
+library("DESeq2")
+library("tximport")
+library(ggplot2)
+
+#Set directory as source file location
+
+#Load metadata
+
+
+
+dir <- system.file("extdata", package="airway")
